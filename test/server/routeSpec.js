ffa56ce26813b52abc86883d17b68cd7021f0a58
Ken Huh <ken123777@gmail.com>
2013-11-23 00:06:55 -0800

    Add tests for unauthenticated users to be able to visit only /login path

--- test/server/routeSpec.js
+++ test/server/routeSpec.js
@@ -1,17 +1,41 @@
 process.env['NODE_ENV'] = 'test';
 
 // Dependencies
-var app = require('../server');
+var app = require('../../server');
+var superagent = require('superagent');
 var chai = require('chai');
 var should = chai.should();
 var expect = chai.expect;
 
 // Server tests
-describe('< Server Tests >', function(){
+describe('< Server Routes Tests >', function(){
 
   describe('Express Application', function(){
     it('should exist', function(){
       expect(app).exist;
+    });
+  });
+
+  describe('Any users', function(){
+    var agent = superagent.agent();
+
+    it('should NOT be able to visit / route', function(done){
+      agent.get('http://localhost:3000')
+      .end(function(err, res){
+        expect(err).not.exist;
+        expect(res.status).equal(302);
+        expect(res.redirects).equal([ 'http://localhost:3000/login' ]);
+        done();
+      });
+    });
+
+    it('should be able to visit /login route', function(done){
+      agent.get('http://localhost:3000/login')
+      .end(function(err, res){
+        expect(err).not.exist;
+        expect(res.status).equal(200);
+        done();
+      });
     });
   });
 
