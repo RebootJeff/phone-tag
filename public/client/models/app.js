6c9a4c4e26ca6d36af9fba53c598e18a5fc593ea
Ryan Roxas <ryanaroxas@gmail.com>
2013-11-23 13:06:41 -0800

    Change app.js to use require.js

--- public/client/models/app.js
+++ public/client/models/app.js
@@ -1,5 +1,9 @@
-var App = Backbone.Model.extend({
-  initialize: function(){
-    console.log("it's initializing!");
-  }
+define(['backbone', 'views/AppView'], function(Backbone, AppView){
+  var App = Backbone.Model.extend({
+    initialize: function(){
+      console.log("it's initializing!");
+      new AppView();
+    }
+  });
+  return App;
 });
