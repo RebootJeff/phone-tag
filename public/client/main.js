7c92e2789c615a422812f887a4dd74dd29f258f2
Ryan Roxas <ryanaroxas@gmail.com>
2013-11-25 09:39:22 -0800

    Remove _=_ in url and disables jquery mobile routing to let backbone handle routing

--- public/client/main.js
+++ public/client/main.js
@@ -16,7 +16,39 @@ require.config({
     }
   }
 });
+require(["jquery", "backbone", "routers/MainRouter", "jqueryMobile"], function($, Backbone, Router){
+  $(document).ready(function(){
+    if (window.location.hash && window.location.hash == '#_=_') {
+      if (window.history && history.pushState) {
+        window.history.pushState("", document.title, window.location.pathname);
+      } else {
+        // Prevent scrolling by storing the page's current scroll offset
+        var scroll = {
+          top: document.body.scrollTop,
+          left: document.body.scrollLeft
+        };
+        window.location.hash = '';
+        // Restore the scroll offset, should be flicker free
+        document.body.scrollTop = scroll.top;
+        document.body.scrollLeft = scroll.left;
+      }
+    }
 
-require(['models/app'], function(App){
-  new App();
+    // Prevents all anchor click handling
+
+    require( [ "jqueryMobile" ], function() {
+      $(document).on( "mobileinit",
+      // Set up the "mobileinit" handler before requiring jQuery Mobile's module
+        function() {
+          // Prevents all anchor click handling including the addition of active button state and alternate link bluring.
+          $.mobile.linkBindingEnabled = false;
+
+          // Disabling this will prevent jQuery Mobile from handling hash changes
+          $.mobile.hashListeningEnabled = false;
+          this.router = new Router();
+
+        }
+      );
+    });
+  });
 });
