e75cc109627dfbba0b58cdf21f783116ae46e3c2
Ryan Roxas <ryanaroxas@gmail.com>
2013-11-25 20:52:34 -0800

    Change file structure to not include client folder

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
 require.config({
   paths: {
-    backbone: '../lib/backbone/backbone',
-    handlebars: '../lib/handlebars/handlebars',
-    jquery: '../lib/jquery/jquery',
-    jqueryMobile: '../lib/jquery-mobile-bower/js/jquery.mobile-1.3.2',
-    underscore: '../lib/underscore/underscore'
+    backbone: 'lib/backbone/backbone',
+    handlebars: 'lib/handlebars/handlebars',
+    jquery: 'lib/jquery/jquery',
+    jqueryMobile: 'lib/jquery-mobile-bower/js/jquery.mobile-1.3.2',
+    underscore: 'lib/underscore/underscore'
   },
   shim: {
     'backbone': {
