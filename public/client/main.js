c49d223b667fe838bd8daf8dd4c6ca73949dc97d
Ryan Roxas <ryanaroxas@gmail.com>
2013-11-25 20:30:56 -0800

    Create an app instance instead of a router instance in main.js

--- public/client/main.js
+++ public/client/main.js
@@ -16,12 +16,13 @@ require.config({
     }
   }
 });
-require(["jquery", "backbone", "routers/MainRouter", "jqueryMobile"], function($, Backbone, Router){
+
+require(["jquery", "backbone", "models/app"], function($, Backbone, App){
   $(document).ready(function(){
     if (window.location.hash && window.location.hash == '#_=_') {
       if (window.history && history.pushState) {
         window.history.pushState("", document.title, window.location.pathname);
-      } else {
+      }else{
         // Prevent scrolling by storing the page's current scroll offset
         var scroll = {
           top: document.body.scrollTop,
@@ -33,22 +34,17 @@ require(["jquery", "backbone", "routers/MainRouter", "jqueryMobile"], function($
         document.body.scrollLeft = scroll.left;
       }
     }
-
-    // Prevents all anchor click handling
-
-    require( [ "jqueryMobile" ], function() {
-      $(document).on( "mobileinit",
-      // Set up the "mobileinit" handler before requiring jQuery Mobile's module
-        function() {
-          // Prevents all anchor click handling including the addition of active button state and alternate link bluring.
-          $.mobile.linkBindingEnabled = false;
-
-          // Disabling this will prevent jQuery Mobile from handling hash changes
-          $.mobile.hashListeningEnabled = false;
-          this.router = new Router();
-
-        }
-      );
-    });
   });
+
+  require(["jqueryMobile"], function(){
+    new App();
+  });
+
+  $(document).bind("mobileinit", function(){
+    // Prevents all anchor click handling including the addition of active button state and alternate link bluring.
+    $.mobile.ajaxEnabled = false;
+    // Disabling this will prevent jQuery Mobile from handling hash changes
+    $.mobile.hashListeningEnabled = false;
+  });
+
 });
