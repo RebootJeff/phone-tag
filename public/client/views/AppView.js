95b3da0ab19917a0711cc531cd5dcb42afc41fae
Ryan Roxas <ryanaroxas@gmail.com>
2013-11-25 20:36:16 -0800

    Set up AppView to listen for click events and uses router.navigate

--- public/client/views/AppView.js
+++ public/client/views/AppView.js
@@ -1,14 +1,39 @@
-define(['backbone','templates/home'], function(Backbone, Template){
+define(['backbone', 'routers/MainRouter'], function(Backbone, Router){
   var AppView = Backbone.View.extend({
 
-    template: Template,
+    el: $('body'),
 
     initialize: function(){
-      this.render();
+      this.router = new Router({el: this.$el.find('#container')});
+      // this.router.on('route', this.updateNav, this);
+      Backbone.history.start({pushState: true});
     },
 
-    render: function(){
-      return this;
+    events: {
+      "click a.home":  "renderHomeView",
+      "click a.leaderboard": "renderLeaderboardView",
+      "click a.game": "renderGameView",
+      "click a.inventory": "renderInventoryView"
+    },
+
+    renderHomeView: function(e){
+      e && e.preventDefault();
+      this.router.navigate('/', {trigger:true});
+    },
+
+    renderLeaderboardView: function(e){
+      e && e.preventDefault();
+      this.router.navigate('/leaderboard', {trigger:true});
+    },
+
+    renderGameView: function(e){
+      e && e.preventDefault();
+      this.router.navigate('/game', {trigger:true});
+    },
+
+    renderInventoryView: function(e){
+      e && e.preventDefault();
+      this.router.navigate('/inventory', {trigger:true});
     }
   });
   return AppView;
