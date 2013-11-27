2671cc9f49ff421fa86bcc28e9dbad5382254adb
Ryan Roxas <ryanaroxas@gmail.com>
2013-11-26 16:10:19 -0800

    Fix routes for single page app. Change redirect url for failure. Remove authorization middleware

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,15 @@
 var mainController = require('../app/controllers/mainController');
-var authorization = require('./middlewares/authorization');
 
 module.exports = function(app, passport){
 
   // GET
-  app.get('/', authorization.authorize, mainController.index);
-  app.get('/login', mainController.login);
+  app.get('/', mainController.index);
+  app.get('/userAuth', mainController.userAuth);
   app.get('/logout', mainController.logout);
 
   // Passport-Facebook
   app.get('/auth/facebook', passport.authenticate('facebook'));
   app.get('/auth/facebook/callback',
     passport.authenticate('facebook', { successRedirect: '/',
-                                      failureRedirect: '/login' }));
+                                      failureRedirect: '/' }));
 };
