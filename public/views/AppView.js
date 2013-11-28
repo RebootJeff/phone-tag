d4eeacf6ee58f3972fc8b4135bdfcf5027f94a1e
Ryan Roxas <ryanaroxas@gmail.com>
2013-11-27 19:14:28 -0800

    Comment out facebook authentication code and use user input authentication

--- public/views/AppView.js
+++ public/views/AppView.js
@@ -5,18 +5,35 @@ define(['backbone', 'routers/MainRouter'], function(Backbone, Router){
 
     initialize: function(){
       this.router = new Router({el: this.$el.find('#container')});
-      this.router.on('route', this.checkAuth, this);
+      this.router.on('route', function(){console.log(this.model.get('username'));}, this);
       this.model.on('loggedIn', this.renderHomeView, this);
       Backbone.history.start({pushState: true});
     },
 
     events: {
-      "click a.home":  "renderHomeView",
-      "click a.leaderboard": "renderLeaderboardView",
-      "click a.game": "renderGameView",
-      "click a.inventory": "renderInventoryView",
-      "click button.tag": "tag",
-      "click #inventory li": "inventory"
+      'submit': 'login',
+      'click a.logout': 'logout',
+      'click a.home':  'renderHomeView',
+      'click a.leaderboard': 'renderLeaderboardView',
+      'click a.game': 'renderGameView',
+      'click a.inventory': 'renderInventoryView',
+      'click button.tag': 'tag',
+      'click #inventory li': 'inventory'
+    },
+
+    login: function(e){
+      e && e.preventDefault();
+      // Todo send it to the server
+      this.model.trigger('createPlayer');
+    },
+
+    logout: function(){
+      // var that = this;
+      // $.get('/logout', function(){
+      //   that.model.set('user', null);
+      //   that.router.navigate('/', {trigger:true});
+      // });
+      this.router.navigate('/', {trigger:true});
     },
 
     renderHomeView: function(e){
@@ -39,20 +56,20 @@ define(['backbone', 'routers/MainRouter'], function(Backbone, Router){
       this.router.navigate('/inventory', {trigger:true});
     },
 
-    checkAuth: function(){
-      if(!this.model.get('user')){
-        this.model.trigger('createPlayer');
-      }
-    },
+    // checkAuth: function(){
+    //   if(!this.model.get('user')){
+    //     this.model.trigger('createPlayer');
+    //   }
+    // },
 
     tag: function(e){
       e && e.preventDefault();
-      console.log("Tag clicked");
+      console.log('Tag clicked');
     },
 
     inventory: function(e){
       e && e.preventDefault();
-      console.log("Inventory is clicked:", e.currentTarget);
+      console.log('Inventory is clicked:', e.currentTarget);
     }
   });
   return AppView;
