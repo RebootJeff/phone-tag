a100098122ba6fd7a8c4864940fa9659fd97cde7
Ryan Roxas <ryanaroxas@gmail.com>
2013-12-02 21:34:35 -0800

    Pass app and socket into the router

--- public/routers/MainRouter.js
+++ public/routers/MainRouter.js
@@ -1,33 +1,33 @@
-define([ "jquery", "backbone", "../views/GameView", "../views/HomeView", "../views/LoginView", "../models/map"], function($, Backbone, GameView, HomeView, LoginView, Map){
+define([ 'jquery', 'backbone', '../views/GameView', '../models/Game', '../views/HomeView', '../views/LoginView', '../views/JoinView', '../models/map'], function($, Backbone, GameView, Game, HomeView, LoginView, JoinView, Map){
   var Router = Backbone.Router.extend({
     initialize: function(options){
-      this.$el = options.el;
+      this.app = options.app;
     },
 
     routes: {
-      "": "login",
-      "home": "home",
-      "leaderboard": "leaderboard",
-      "game": "game",
-      "inventory": "inventory"
-    },
-
-    swapView: function(view){
-      this.$el.html(view.render().el);
+      '': 'login',
+      'home': 'home',
+      'leaderboard': 'leaderboard',
+      'join': 'join',
+      'game': 'game',
+      'inventory': 'inventory'
     },
 
     login: function(){
-      var loginView = new LoginView();
-      this.swapView(loginView);
+      new LoginView();
     },
 
     home: function(){
       if($('#home').length === 0){
-        var homeView = new HomeView();
-        this.swapView(homeView);
+        new HomeView();
       }else{
         this.slidePageFrom($('#leaderboard'), $('#home'), 'left');
       }
+    },
+
+    join: function(){
+      this.app.set('game', new Game({currentPlayer: this.app.get('user')}));
+      new JoinView({model: this.app.get('game'), user: this.app.get('user')});
     },
 
     leaderboard: function(){
@@ -36,9 +36,11 @@ define([ "jquery", "backbone", "../views/GameView", "../views/HomeView", "../vie
 
     game: function(){
       if($('#game').length === 0){
-        var gameView = new GameView();
-        this.swapView(gameView);
-        new Map();
+        var that = this;
+        this.app.get('game').socket.on('gameStarting', function(){
+          new GameView({model: that.app.get('game'), socket: that.app.get('game').socket});
+        });
+        this.app.get('game').socket.emit('startGame');
       }else{
         this.slidePageFrom($('#inventory'), $('#game'), 'left');
       }
