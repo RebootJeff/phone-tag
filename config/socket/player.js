c6ac8b1915728ab867c8f5c84e6e62d4aa2ebaff
Ryan Roxas <ryanaroxas@gmail.com>
2013-12-02 21:28:45 -0800

    Refactor player.js to refer to socket id instead of socket connection. Remove setLocation function

--- config/socket/player.js
+++ config/socket/player.js
@@ -2,8 +2,7 @@ var Player = function(socket, user, room) {
   this.name = user;
   this.room = room;
   this.location = {};
-  this.socket = socket;
-  this.user = user;
+  this.socketID = socket.id;
   this.score = 0;
   this.team = null;
   this.playerSight = 0;
@@ -21,12 +20,6 @@ var Player = function(socket, user, room) {
   //game statistics
   this.kills = 0;
   this.deaths = 0;
-};
-
-Player.prototype.setLocation = function(lat, lon) {
-  this.lat = lat;
-  this.lon = lon;
-  this.location = {lat: this.lat, lon: this.lon };
 };
 
 Player.prototype.setPowerup = function(powerUp) {
