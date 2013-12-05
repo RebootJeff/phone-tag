56f28d5c8823742bff48a6e2607b70b1ac668d53
Jeff Lee <leejeffc@gmail.com>
2013-12-04 22:24:25 -0800

    Convert templates into JS to avoid RequireJS issues

---
+++ public/templates/join.js
@@ -0,0 +1,9 @@
+define(function(){
+  return "<h1>Game</h1>" +
+  "<table>" +
+    "<tr>" +
+      "<th>Players</th>" +
+    "</tr>" +
+  "</table>" +
+  "<button class='start'>Start!</button>";
+});
